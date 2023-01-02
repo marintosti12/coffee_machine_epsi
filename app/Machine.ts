a475f416531472bdd02925d240e48ca289ca4dba
Marin Tostivint <marin.tostivint@epsi.fr>
2023-01-02 15:27:09 +0100

    add Test 3

--- app/Machine.ts
+++ app/Machine.ts
@@ -1,16 +1,19 @@
 export class Machine {
     nombreCafes : number;
     sommeArgent : number;
+    eau: number;
 
-    constructor() {
+    constructor(eau: number) {
         this.nombreCafes = 0;
         this.sommeArgent = 0;
+        this.eau = eau;
     }
 
     inserer(sommeInseree: number) : void {
-        if (sommeInseree == 0.40) {
+        if (sommeInseree == 0.40 && this.eau >= 0.15) {
             this.sommeArgent += sommeInseree;
             this.nombreCafes++;
+            this.eau -= 0.15;
         }
     }
 
@@ -20,5 +23,9 @@ export class Machine {
 
     getArgentEncaisse() : number {
         return this.sommeArgent;
+    }
+
+    getEauRestante(): number {
+        return this.eau;
     }
 }
